349457eeb8bbb05034833317d4ba4febc7200912
mahdisouki <100594801+mahdisouki@users.noreply.github.com>
2023-10-18 23:22:23 +0100

    Update server.js

--- cosserplus/server/server.js
+++ cosserplus/server/server.js
@@ -11,7 +11,9 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());  
-app.use(cors({origin : "http://localhost:3000"}));
+app.use(cors({ origin:'http://localhost:3000', 
+    credentials:true, 
+    optionSuccessStatus:200}));
 app.options('*', cors());
 app.get('/test' , (req,res)=>{
    res.json('test') 
