c2a42cad0ff10f86009c03955918056f3939d9f2
mahdisouki <100594801+mahdisouki@users.noreply.github.com>
2023-10-18 21:53:43 +0100

    Update server.js

--- cosserplus/server/server.js
+++ cosserplus/server/server.js
@@ -6,13 +6,12 @@ var bodyParser = require('body-parser')
 const userRoutes = require('./routes/User')
 const fileRoutes = require('./routes/Files');
 const validateToken = require("./middlewares/validateTokenHandler");
-var cors = require('cors')
-
+var cors = require('cors');
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());  
-app.use(cors({origin:"http://localhost:3000" , credentials:true}));
+app.use(cors({origin:["http://localhost:3000"] , credentials:true}));
 app.options('*', cors());
 app.get('/test' , (req,res)=>{
    res.json('test') 
