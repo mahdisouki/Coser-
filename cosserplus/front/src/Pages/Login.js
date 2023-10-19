5696939bd10b5e351ad79197bda54d3761e5e27f
mahdisouki <100594801+mahdisouki@users.noreply.github.com>
2023-10-19 21:37:06 +0100

    Update Login.js

--- cosserplus/front/src/Pages/Login.js
+++ cosserplus/front/src/Pages/Login.js
@@ -21,7 +21,7 @@ const onSubmit = (e)=>{
       password : Motpasse
 
     }
-    axios.post("https://azer-htjc6d7zb-mahdisouki-s-team.vercel.app/user/login" , data).then(
+    axios.post("azer-nine.vercel.app/user/login" , data).then(
       res=>{
         console.log("response",res.data.accesstoken)
         localStorage.setItem('isLogged' , true)
