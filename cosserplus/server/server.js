ee05a595fabd763543b13b30bd610ecbd05bd185
mahdisouki <100594801+mahdisouki@users.noreply.github.com>
2023-11-04 01:06:03 +0100

    Update server.js

--- cosserplus/server/server.js
+++ cosserplus/server/server.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());  
-app.use(cors({ origin:['https://client-flax-psi.vercel.app'], 
+app.use(cors({ origin:['https://www.coserplus.com/'], 
     credentials:true, 
     optionSuccessStatus:200}));
 app.options('*', cors());
