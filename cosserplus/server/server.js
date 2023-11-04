4ab355a003bf1b98b22fd9c4fa46f8740fe4438a
mahdisouki <100594801+mahdisouki@users.noreply.github.com>
2023-11-04 01:12:33 +0100

    Update server.js

--- cosserplus/server/server.js
+++ cosserplus/server/server.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());  
-app.use(cors({ origin:['https://www.coserplus.com/'], 
+app.use(cors({ origin:['https://www.coserplus.com'], 
     credentials:true, 
     optionSuccessStatus:200}));
 app.options('*', cors());
